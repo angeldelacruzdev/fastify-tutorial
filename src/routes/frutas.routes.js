88884d17df092346d3bfb43b8185704dc71f7882
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2021-09-29 16:07:54 -0400

    Buscar frutas por rango de precios

--- src/routes/frutas.routes.js
+++ src/routes/frutas.routes.js
@@ -12,6 +12,11 @@ const frutasRoutes = [
     handler: frutaCtrl.getFruta,
   },
   {
+    url: "/frutas/price-range",
+    method: "GET",
+    handler: frutaCtrl.getFrutaPriceRange,
+  },
+  {
     url: "/frutas",
     method: "POST",
     handler: frutaCtrl.createFruta,
